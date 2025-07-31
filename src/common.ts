6e5724ef5f6b948c08acc49a2b96acdc398183b8
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-07-31 18:34:53 -0300

    Adds basic types to provide options to validators.

---
+++ src/common.ts
@@ -0,0 +1,81 @@
+/**
+ * Contains the data that is always present on translation
+ * options and can be used in all translations.
+ * These translations are always invoked upon a failing constraint.
+ */
+export interface I18nTranslationOptions {
+  /**
+   * The name of the failed constraint
+   */
+  constraint: string
+
+  /**
+   * The name of the field that failed the constraint
+   */
+  field: string | symbol
+
+  /**
+   * The value of the field that failed the constraint
+   */
+  value: unknown
+}
+
+/**
+ * Minimal interface to work with internationalisation.
+ * This mimics `ember-intl`'s API, so it is pretty much
+ * plug-and-play.
+ */
+export interface I18nHandler {
+  /**
+   * Checks whether an error key is recognised by the underlying
+   * internationalisation engine
+   * @param errorKey The key under which a translation might be
+   * available
+   * @see messageForError
+   */
+  exists(errorKey: string): boolean
+
+  /**
+   * Fetches a translation from the internationalisation engine
+   * and returns it.
+   * @param errorKey The error key associated with the desired
+   * error message
+   * @param options An object with some information about the
+   * failed constraint context, along with failed validation
+   * options.
+   * @see I18nTranslationOptions
+   */
+  t<Options extends I18nTranslationOptions>(
+    errorKey: string,
+    options: Options
+  ): string
+}
+
+/**
+ * Base interface for validation options passed to validators.
+ * These options are also used on error handling when a constraint
+ * is not met.
+ * @see messageForError
+ */
+export interface ValidationOptions {
+  /**
+   * A custom message that will always be returned on a failing
+   * constraint, taking precedence over other
+   */
+  message?: string
+
+  /**
+   * An arbitrary key to be provided to the internationalisation
+   * engine, if present, to fetch a specific translation.
+   * @see I18nHandler.t
+   */
+  translationKey?: string
+
+  /**
+   * An interface of the internationalisation engine used to
+   * translate error messages if present.
+   * This mimics `ember-intl`'s interface, so the service can be
+   * directly provided as this property.
+   */
+  i18nHandler?: I18nHandler
+}
