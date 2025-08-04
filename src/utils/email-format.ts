584403d516c8fa9d00a466c1c9d84f73987b038a
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-08-04 12:53:31 -0300

    Adds utilities to validate e-mails and UUIDs

---
+++ src/utils/email-format.ts
@@ -0,0 +1,10 @@
+/**
+ * Checks if the given string contains a valid e-mail address.
+ * @param value
+ */
+export function isEmailValid(value: string) {
+  const emailRegex =
+    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
+
+  return value.match(emailRegex) !== null
+}
---
+++ src/utils/uuid-format.ts
@@ -0,0 +1,13 @@
+const UUID_FORMAT = /^[0-9a-f]{12}[1-5][0-9a-f]{3}[89ab][0-9a-f]{15}$/i
+
+/**
+ * Checks whether a string contains a valid UUID.
+ * Before testing, the value is stripped from dashes, periods and whitespaces.
+ * @param value The string to be tested
+ * @return Whether the provided string contains a valid UUID
+ */
+export function isUUIDValid(value: string) {
+  const sanitised =
+    value.replaceAll(/\s|-|\./g, '')
+  return UUID_FORMAT.test(sanitised)
+}
