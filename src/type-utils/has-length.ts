389fc389d7f3f9b6f8425de8d4bce7604b2294a5
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-08-04 12:53:47 -0300

    Adds type utility to check if something has a numeric `length` property.

---
+++ src/type-utils/has-length.ts
@@ -0,0 +1,12 @@
+/**
+ * Checks whether a value has a numeric `length` property
+ * @param value
+ */
+export function hasLength(value: unknown): value is { length: number } {
+  return typeof value === 'string' || (
+    typeof value === 'object' &&
+    value !== null &&
+    'length' in value &&
+    typeof value.length === 'number'
+  )
+}
